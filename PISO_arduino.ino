12f91995eb88527a1ca9f7afe55f17c6f7da1382
bhargavCSSE <42753135+bhargavCSSE@users.noreply.github.com>
2019-01-17 12:08:32 -0600

    Update PISO_arduino.ino

--- PISO_arduino.ino
+++ PISO_arduino.ino
@@ -1,70 +1,35 @@
-const int data_pin = 11; 
-const int shld_pin = 8; 
-const int clk_pin = 12; 
-const int ce_pin = 9;
 byte ibyte = B00000000;
+byte ibyte2 = B11111111;
 byte mask = B00000000;
-int i;
+int i,j;
 int pins[] = {36, 34, 32, 30, 37, 35, 33, 31};
+int pins2[] = {46, 44, 42, 40, 47, 45, 43, 41};
 byte incoming;
 
 void setup() 
 {                
   Serial.begin(9600);
-  pinMode(30,OUTPUT);
-  pinMode(31,OUTPUT);
-  pinMode(32,OUTPUT);
-  pinMode(33,OUTPUT);
-  pinMode(34,OUTPUT);
-  pinMode(35,OUTPUT);
-  pinMode(36,OUTPUT);
-  pinMode(37,OUTPUT);
-  
-  pinMode(shld_pin, OUTPUT);
-  pinMode(ce_pin, OUTPUT);
-  pinMode(clk_pin, OUTPUT);
-  pinMode(data_pin, INPUT);
-
-  digitalWrite(clk_pin, HIGH);
-  digitalWrite(shld_pin, HIGH);
-
+  for(i=0;i<8;i++){
+    pinMode(30+i,OUTPUT);
+    pinMode(40+i,OUTPUT);
+  }
 }
 
 void loop() {
   mask = B10000000;
   for(i=0; i<=7; i++){
     digitalWrite(pins[i], HIGH && (ibyte & mask));
+    digitalWrite(pins2[i], HIGH && (ibyte2 & mask));
     mask = mask >> 1;
     if(ibyte == B11111111){
       ibyte = B00000000;
-      Serial.print("\n\nBinary value will be reset to 00000000\n");
-      delay(2000);
     }
   }
   ibyte = ibyte + 1;
-  incoming = read_shift_regs();
+  ibyte2 = 255 - ibyte;
   Serial.print("\nABCDEFGH : ");
-  print_byte(incoming);
-  delay(50); 
-}
-
-byte read_shift_regs()
-{
-  byte the_shifted = 0;  
-  digitalWrite(shld_pin, LOW);
-  delayMicroseconds(5);
-  digitalWrite(shld_pin, HIGH);
-  delayMicroseconds(5);
-
-  pinMode(clk_pin, OUTPUT);
-  pinMode(data_pin, INPUT);
-  digitalWrite(clk_pin, HIGH);
-  digitalWrite(ce_pin, LOW);
-
-  the_shifted = shiftIn(data_pin, clk_pin, MSBFIRST);
-  digitalWrite(ce_pin, HIGH);
-
-  return the_shifted;
+  print_byte(ibyte);
+  delay(500); 
 }
 
 void print_byte(byte val)
@@ -75,4 +40,3 @@ void print_byte(byte val)
       Serial.print(val >> i & 1, BIN);
     }
 }
-
